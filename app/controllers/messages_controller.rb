2fd2b5734f438c809c2178ce884cc21565fc80e7
behzad <behzadfareed@gmail.com>
2020-10-03 19:13:47 +0330

    pusher

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,12 @@
 require 'pusher'
 
+pusher = Pusher::Client.new(
+  app_id: "735558",
+  key: "2a0e191a544c745780f0",
+  secret: "7fa58d449883d3e9c3bc",
+  cluster: "ap2",
+  use_tls: true
+)
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :update, :destroy]
 
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,5 +1,12 @@
 require 'pusher'
 
+pusher = Pusher::Client.new(
+  app_id: "735558",
+  key: "2a0e191a544c745780f0",
+  secret: "7fa58d449883d3e9c3bc",
+  cluster: "ap2",
+  use_tls: true
+)
 class Contract < ApplicationRecord
   belongs_to :user, class_name: 'User', optional: true
   belongs_to :customer, class_name: 'User', optional: true
