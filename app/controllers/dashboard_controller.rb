5d97e439643efe047cd895269c278fe80e26ebe3
behzad <behzadfareed@gmail.com>
2020-12-05 21:39:06 +0330

    ss

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -73,8 +73,8 @@ class DashboardController < ApplicationController
       finish_date: c.contract.finish_date.to_date.to_pdate.to_s, 
       contract_number: c.contract.contract_number, 
       building_number: c.contract.building_number,
-      customer: c.customer.full_name,
-      cell: c.customer.cell,
+      customer: c.contract.customer.full_name,
+      cell: c.contract.customer.cell,
     }}
 
     @due_standards = Elevator.where('standard_finish_date < ?', Date.today + 1.month)
@@ -83,17 +83,17 @@ class DashboardController < ApplicationController
       finish_date: c.contract.finish_date.to_date.to_pdate.to_s, 
       contract_number: c.contract.contract_number, 
       building_number: c.contract.building_number,
-      customer: c.customer.full_name,
-      cell: c.customer.cell,
+      customer: c.contract.customer.full_name,
+      cell: c.contract.customer.cell,
     }}   
      @no_standards = Elevator.where(standard: nil)
      .map{|c| {
-       id: c.contract.id, 
-       finish_date: c.contract.finish_date.to_date.to_pdate.to_s, 
-       contract_number: c.contract.contract_number, 
-       building_number: c.contract.building_number,
-       customer: c.customer.full_name,
-      cell: c.customer.cell,
+      id: c.contract.id, 
+      finish_date: c.contract.finish_date.to_date.to_pdate.to_s, 
+      contract_number: c.contract.contract_number, 
+      building_number: c.contract.building_number,
+      customer: c.contract.customer.full_name,
+      cell: c.contract.customer.cell,
      }}  
     @no_insurances = Elevator.where(insurance: nil)
     .map{|c| {
@@ -101,8 +101,8 @@ class DashboardController < ApplicationController
       finish_date: c.contract.finish_date.to_date.to_pdate.to_s, 
       contract_number: c.contract.contract_number, 
       building_number: c.contract.building_number,
-      customer: c.customer.full_name,
-      cell: c.customer.cell,
+      customer: c.contract.customer.full_name,
+      cell: c.contract.customer.cell,
     }}  
     
     render json: {
